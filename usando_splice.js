71400c589cf7366594e7986a56e75992dd28b8a5
gimenezandrea98 <gimenezandrealorena1998@gmail.com>
2024-01-23 19:38:08 -0300

    tareas

--- usando_splice.js
+++ usando_splice.js
@@ -11,6 +11,16 @@ que va a reemplazar lo que estaba
 arrDefiguras.splice(2, 0, "cuadrado", "trapezoide");
 console.log(arrDefiguras);
 
+// el experimento de traer solamente
+// "circulo", "triangulo" y "cuadrado"
+
+//debugger;
+//const cortadito_1 = arrDefiguras.splice(0,2);
+//const cortadito_2 = arrDefiguras[0];
+//const mezclar = cortadito_1.concat(cortadito_2);
+//debugger;
+
+
 //borrar elementos
 arrDefiguras.splice(2,3,"rombo")
 console.log(arrDefiguras);
