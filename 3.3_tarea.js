67787b007cf4ca69e034e74206848f0d463550d4
gimenezandrea98 <gimenezandrealorena1998@gmail.com>
2024-01-25 19:36:51 -0300

    tarea 3.3

---
+++ 3.3_tarea.js
@@ -0,0 +1,8 @@
+// Array original o inicial
+const original_array = [1, 2, 3]
+
+// Anidar el array original en un nuevo array tres veces
+const new_array = [original_array, original_array, original_array]
+
+// Mostrar en la consola el valor 2 de uno de los arrays anidados
+console.log(new_array[1][1])
